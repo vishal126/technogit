3b7622989e0b487bb3492f8b080fe56d26084e6c
Vishal <vishalmenaria86@gmail.com>
2024-08-26 17:19:53 +0530

    msg

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log('git version 1');
+console.log('git version 2 new file');
+
+//git init
+//git status
+//git add filename/.
+//git commit -m "msg"
+
+// note to check all version of code we have "git log --all"
+// to initialize to git git log --all
+// to shift between commits "git checkout "commit-hash""
+
+// git remote add origin https://github.com/vishal126/technogit.git
+
+// git push --set-upstream origin master
+
+// git clone https://github.com/vishal126/technogit.git
